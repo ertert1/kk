10cc4500dd07390a0f2bab136958ffbfaf8cb6ac
ertert1 <owllwoowllwo@mail.ru>
2018-04-27 01:10:02 +0600

    k

---
+++ src/main/java/kr/KR/Calculation.java
@@ -0,0 +1,12 @@
+package kr.KR;
+/**
+ * Класс для расчётов
+ */
+public class Calculation {
+    public static float sums(float k1, float r1) {
+    	float sum;
+    	sum = (float) ((k1*1.2)/r1);
+    	return sum;
+    }
+}
+
